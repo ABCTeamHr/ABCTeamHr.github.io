2e3791072dab77315162f8b05724c6ae0b83ab8f
Dima <schtanckevitch.dima@yandex.ru>
2022-12-27 12:24:30 +0300

    добавил модальное окно для десктопа хрома, буду тестировать мобильные экраны

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+let label = document.querySelectorAll('.modal__close')
+let modal = document.querySelectorAll('.modal')
+
+label[0].addEventListener('click', function () {
+    modal[0].remove()
+    document.body.style.overflowY = 'visible'
+});
