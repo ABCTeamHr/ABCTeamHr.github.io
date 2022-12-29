35e08046bb9c08a27183f29ccbcabe359659ad43
Dima <schtanckevitch.dima@yandex.ru>
2022-12-30 00:18:12 +0300

    add close button(mobile) + fix block + add button link

--- js/modal.js
+++ js/modal.js
@@ -2,16 +2,23 @@ let label = document.querySelectorAll('.modal__close')
 let modal = document.querySelectorAll('.modal')
 
 label[0].addEventListener('click', function () {
-    modal[0].remove()
+    modal[0].remove();
+    mobile_modal[0].remove();
+
     document.body.style.overflowY = 'visible'
     //  добавить правильную позицию для боди 
     document.body.style.position = 'static'
 });
 
-let windows = document.querySelectorAll('html')
+const mobile_close = document.querySelectorAll('.mobile_close')
+const mobile_modal = document.querySelectorAll('.mobile_modal')
 
+console.dir(mobile_modal);
 
-
-window.addEventListener('resize', function() {
-    console.dir(windows[0].clientWidth)
+mobile_close[0].addEventListener('click', function() {
+    mobile_modal[0].remove();
+    modal[0].remove();
+    document.body.style.overflowY = 'visible'
+    //  добавить правильную позицию для боди 
+    document.body.style.position = 'static'
 })
