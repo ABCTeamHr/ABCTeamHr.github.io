4c5eeb027ab942dbd8073a5b81b0216a68378608
Dima <schtanckevitch.dima@yandex.ru>
2023-01-09 12:26:43 +0300

    удалил лишние слушатели событий, написал логику для функций которые удаляют слушатели

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,30 @@
 let label = document.querySelectorAll('.modal__close')
 let modal = document.querySelectorAll('.modal')
-
-label[0].addEventListener('click', function () {
-    modal[0].remove();
-    mobile_modal[0].remove();
-
-    document.body.style.overflowY = 'visible'
-    //  добавить правильную позицию для боди 
-    document.body.style.position = 'static'
-});
-
 const mobile_close = document.querySelectorAll('.mobile_close')
 const mobile_modal = document.querySelectorAll('.mobile_modal')
 
-console.dir(mobile_modal);
+const desctopModalClose = () => {
+    mobile_close[0].removeEventListener('click', mobileModalClose)
+    modal[0].remove();
+    mobile_modal[0].remove();
 
-mobile_close[0].addEventListener('click', function() {
+    document.body.style.overflowY = 'visible'
+    //  добавить правильную позицию для боди 
+    document.body.style.position = 'static'
+}
+
+label[0].addEventListener('click', desctopModalClose, {once: true});
+
+const mobileModalClose = () => {
+    label[0].removeEventListener('click', desctopModalClose);
     mobile_modal[0].remove();
     modal[0].remove();
     document.body.style.overflowY = 'visible'
     //  добавить правильную позицию для боди 
     document.body.style.position = 'static'
-})
+}
+
+mobile_close[0].addEventListener('click', mobileModalClose, {once:true})
+
+// console.dir(mobile_modal);
+
